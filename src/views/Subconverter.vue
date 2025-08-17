e9d21041768895b92d26a8ae4a4ab5109732105a
Yiliang Dai <1139523599@qq.com>
2025-08-17 18:11:20 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,24 +338,20 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "BM猫云后端【vless reality+hy1+hy2】": "https://subapi.916300.xyz",
+          "BM-Phala后端【vless reality+hy1+hy2】": "https://37236c0a06029f4cfb3a74f95fb167176952734e-15051.dstack-prod8.phala.network/",
+          "BM猫云后端【vless reality+hy1+hy2】": "https://xcitlwztxuar.cloud.cloudcat.one",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
-          { value: "https://subapi.916300.xyz" },
+          { value: "https://37236c0a06029f4cfb3a74f95fb167176952734e-15051.dstack-prod8.phala.network/" },
+          { value: "https://xcitlwztxuar.cloud.cloudcat.one" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
           { value: "https://sub.d1.mk" },
-          { value: "https://www.nameless13.com" },
-          { value: "https://sub.xeton.dev" },
-          { value: "https://api.wcc.best" },
         ],
         remoteConfig: [
           {
@@ -1312,3 +1308,4 @@ export default {
   }
 };
 </script>
+
